d0f222025840e8506e39fa258af591b854a7b25e
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-04-14 13:47:14 +0200

    403

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,11 +1,13 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
 
         String result;
+        int result2, result3;
 
         SimpleUser simpleUser = new SimpleUser("Adam");
 
@@ -13,6 +15,17 @@ public class TestingMain {
 
         if (result.equals("Adam")) {
             System.out.println("Test OK");
+        } else {
+            System.out.println("Bad result");
+        }
+
+        Calculator calculator = new Calculator();
+
+        result2 = calculator.add(2, 4);
+        result3 = calculator.subtract(10, 5);
+
+        if (result2 == 6 & result3 == 5) {
+            System.out.println("Calculator test OK");
         } else {
             System.out.println("Bad result");
         }
